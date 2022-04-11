d0eab2977fcfeae421849582935ac6de7dc20911
YUMAxim <yu1366mak19@gmail.com>
2022-04-12 01:13:38 +0900

    add register,login,logout,homepage button

--- src/resources/views/nav.blade.php
+++ src/resources/views/nav.blade.php
@@ -1,11 +1,43 @@
 <nav>
     <a href="/">Peach</a>
     <ul>
-        <li>
-            <a href="{{ route('requests.create') }}">募集する</a>
+
+        @guest
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
         </li>
-        <li>
-            <a href="">マイページ</a>
+        @endguest
+
+        @guest
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">ログイン</a>
         </li>
+        @endguest
+
+        @auth
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('requests.create') }}">募集する</a>
+        </li>
+        @endauth
+
+        @auth
+        <!-- Dropdown -->
+        <li class="nav-item dropdown">
+            <a class="nav-link">アイコン:)</a>
+            <div class="">
+{{--                 <button class="" type="button" onclick="location.href='{{ route("users.show", ["name" => Auth::user()->name]) }}'">
+                    マイページ
+                </button> --}}
+                <button form="logout-button" class="" type="submit">
+                    ログアウト
+                </button>
+            </div>
+        </li>
+        <form id="logout-button" method="POST" action="{{ route('logout') }}">
+            @csrf
+        </form>
+        <!-- Dropdown -->
+        @endauth
+
     </ul>
 </nav>
