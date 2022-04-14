6e6f64e7590b1eebc135f536865db7668958227b
YUMAxim <yu1366mak19@gmail.com>
2022-04-14 16:42:01 +0900

    fix: split @categories

---
+++ src/app/Http/Controllers/RecruitSelectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use Illuminate\Support\Collection;
+
+class RecruitSelectController extends Controller
+{
+    public function categories(): Collection
+    {
+        $categories = Category::select('name')->get();
+        $categories_name = $categories->pluck('name');
+        $categories_name->prepend('カテゴリーを選択してください');
+        return $categories_name;
+    }
+}
