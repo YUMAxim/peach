6c2f1a14ee92a11a91df2c9a7c86d9e5d4221852
YUMAxim <yu1366mak19@gmail.com>
2022-04-14 12:23:43 +0900

    update: composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ab06617e903bd63d4a58baa0c22e694f8ef1c6f6',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ab06617e903bd63d4a58baa0c22e694f8ef1c6f6',
+            'dev_requirement' => false,
+        ),
+    ),
+);
